2130a7f75fb25216a9297c6274f25487db4b485e
ilya <zapaska96@gmail.com>
2022-11-16 22:25:46 +0500

    first

---
+++ wwwroot/testDriveForm.js
@@ -0,0 +1,18 @@
+const onSubmit = () => {
+
+    const mail = document.getElementById('email')
+    const phone = document.getElementById('phone')
+
+    if (document.getElementById('saloon').value !== 'г. Москва' && document.getElementById('saloon').value !== 'г. Санкт-Петербург') {
+        alert('Неправильно введен салон')
+        return false
+    } 
+
+
+    if (phone.value.length !== 12 && phone.value[0] !== "+") {
+        alert('Неправильно введен номер телефона')
+        return false
+    }
+
+    window.open('mailto:' + mail.value)
+}
